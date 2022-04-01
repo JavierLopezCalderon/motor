1b5fac28267634f8a0c5e908b9fcf98aa1bc4e67
JAVIER LóPEZ CALDERóN <75862739+JavierLopezCalderon@users.noreply.github.com>
2022-04-01 14:02:16 -0600

    eeprom y contador

--- motor/motor.ino
+++ motor/motor.ino
@@ -1,9 +1,15 @@
 // Define stepper motor connections:
 #include <Wire.h> 
 #include <LiquidCrystal_I2C.h>
+#include <EEPROM.h>
 
 #define dirPin 2
 #define stepPin 3
+#define Botonreset 4 
+int contador;
+int pasos = 120, timedelay = 20;
+int limiteContador = 1000;
+int fin;
 
 LiquidCrystal_I2C lcd(0x27, 16, 2);  //
 
@@ -11,38 +17,62 @@ void setup() {
   // Declare pins as output:
   pinMode(stepPin, OUTPUT);
   pinMode(dirPin, OUTPUT);
-  
+  pinMode(Botonreset, INPUT);
   lcd.init();
   lcd.backlight();
   lcd.print("INICIANDO...");
   delay(1000);
   lcd.clear();
-  /*// Set the spinning direction CW/CCW:
-  digitalWrite(dirPin, HIGH);*/
+  EEPROM.get(0, contador);
 }
 
 void loop() {
-  digitalWrite(dirPin, HIGH);
-  // These four lines result in 1 step:
-  for(int i=0; i<300; i++){
-    digitalWrite(stepPin, HIGH);
-    delay(20);
-    digitalWrite(stepPin, LOW);
-    delay(20);
-    lcd.setCursor(0, 0);
-    lcd.print("P:");
-    lcd.setCursor(3, 0);
-    lcd.print(i);
-   // lcd.clear();
+  while(contador < limiteContador+1){ 
+    lcd.clear();
+    lcd_contador();
+    digitalWrite(dirPin, HIGH);
+    ciclopaso();
+    lcd.clear();
+    lcd_contador();
+    digitalWrite(dirPin, LOW);
+    ciclopaso();
+    contador++;
+    EEPROM.put(0, contador);
   }
-  digitalWrite(dirPin, LOW);
-  // These four lines result in 1 step:
-  for(int i=0; i<1500; i++){
-    digitalWrite(stepPin, HIGH);
-    delay(20);
-    digitalWrite(stepPin, LOW);
-    delay(20);
+  if(contador =< limiteContador && fin == 0){
+    lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("FIN");
+  lcd.setCursor(5, 0);
+  lcd.print("C:");
+  lcd.setCursor(8,0);
+  lcd.print(contador);
+    fin = 1;
   }
+  if(digitalRead(Botonreset) == HIGH){
+    contador =0;
+    fin =0;
+    EEPROM.put(0, contador);
+    delay(500);
+  } 
+}
 
-  
+void lcd_contador(){
+  lcd.setCursor(0, 0);
+  lcd.print("C:");
+  lcd.setCursor(3, 0);
+  lcd.print(contador);
+}
+
+void ciclopaso(){
+  for(int i=0; i<pasos+1; i++){
+    digitalWrite(stepPin, HIGH);
+    delay(timedelay);
+    digitalWrite(stepPin, LOW);
+    delay(timedelay);
+    lcd.setCursor(6, 0);
+    lcd.print("P:");
+    lcd.setCursor(8, 0);
+    lcd.print(i);
+  }
 }
